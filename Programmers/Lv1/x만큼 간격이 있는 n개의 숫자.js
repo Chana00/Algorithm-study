8e85baeb0db6cbab9e3a3d4cc4b64974ca797cd6
고예원 <yewon.ko@hancom.com>
2022-11-17 13:35:26 +0900

    [Programmers][Lv1][JS] x간격의 n개의 숫자

---
+++ Programmers/Lv1/x만큼 간격이 있는 n개의 숫자.js
@@ -0,0 +1,9 @@
+function solution(x, n) {
+    var answer = [];
+    for(i=1; i<=n; i++) {
+        answer.push(x*i)
+    }
+    return answer;
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12954
