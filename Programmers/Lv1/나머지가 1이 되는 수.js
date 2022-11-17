1db19be7597476ce86dc847d4c952f49b8c0904e
고예원 <yewon.ko@hancom.com>
2022-11-17 13:50:49 +0900

    [Programmers][Lv1][JS] 나머지가 1이 되는 수

---
+++ Programmers/Lv1/나머지가 1이 되는 수.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let i = 2;
+    while(1) {
+        if(n%i === 1) return i;
+        else i++;
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/87389
