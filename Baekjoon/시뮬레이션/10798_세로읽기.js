fabded089a4122377e3f5bfa3db263bc5e6748b5
고예원 <yewon.ko@hancom.com>
2023-01-10 17:15:08 +0900

    [BJ][시뮬레이션] 10798_세로읽기 & 레이아웃 세팅

---
+++ Baekjoon/시뮬레이션/10798_세로읽기.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+/* 문제풀이 */
+
+console.log(input) // 확인 후 지우기
+
+// 백준에서 JS를 풀기위한 코드 레이아웃
+// \r은 무시해도 된다.
---
+++ Programmers/Lv2/카펫.js
@@ -0,0 +1,14 @@
+function solution(brown, yellow) {
+    var answer = [];
+    let arr = [];
+    // 노랑의 약수를 구한다
+    // for (let i = yellow; i > 0; i--) {
+    //     if ((yellow % i) == 0) arr.push(i);
+    // }
+    let sum = brown + yellow;
+    return answer;
+}
+
+solution(10, 2)
+solution(8, 1)
+solution(24, 24)
